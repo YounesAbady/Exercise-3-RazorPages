79c109d4c4bbdf7e57d2cf6b14920b0999342d3e
YounesAbady <102363875+YounesAbady@users.noreply.github.com>
2022-08-01 00:23:16 +0200

    Added Swagger

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -7,8 +7,15 @@ builder.Services.AddSingleton<RecipeController>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddMvc();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Recipe API",
+        Description = "Api for exercise-3"
+    });
+});
 
 
 var app = builder.Build();
@@ -17,7 +24,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Recipe API");
+        c.RoutePrefix = "";
+    });
 }
 
 app.UseHttpsRedirection();
